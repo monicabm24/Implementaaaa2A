1e28ad311cccfbf021a30ead3eae70d4cd0f4f4c
monicabm24 <monicabmendoza24@gmail.com>
2025-03-19 11:56:29 -0600

    Add files via upload

---
+++ programa 4.py
@@ -0,0 +1,5 @@
+peso_payaso = 112
+peso_muñeca = 75
+num_payasos = int(input("Ingrese el número de payasos vendidos: "))
+num_muñecas = int(input("Ingrese el número de muñecas vendidas: "))
+peso_total = num_payasos * peso_payaso + num_muñecas * peso_muñeca print("El peso total del paquete es:", peso_total, "g")
